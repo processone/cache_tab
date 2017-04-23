1a8644460b5f84e7582f9487d4a3a1ee80ab28b0
Jeet Parmar <jparmar@redhat.com>
2017-04-24 01:15:50 +0530

    add update feature to ets_cache

--- src/ets_cache.erl
+++ src/ets_cache.erl
@@ -26,7 +26,7 @@
 %% API
 -export([new/1, new/2, start_link/2, delete/1, delete/2, delete/3,
 	 lookup/2, lookup/3, insert/3, insert/4, insert_new/3, insert_new/4,
-	 setopts/2, clear/1, clear/2, filter/2, all/0, info/1, info/2]).
+	 update/3, setopts/2, clear/1, clear/2, filter/2, all/0, info/1, info/2]).
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
 	 terminate/2, code_change/3]).
@@ -132,6 +132,33 @@ insert_new(Name, Key, Val, Nodes) ->
 				      ?CALL_TIMEOUT)
 	      end, false, Nodes)
     end.
+
+-spec update(atom(), any(), read_fun()) -> {ok, any()} | error.
+update(Name, Key, UpdateFun) ->
+  Value = UpdateFun(),
+  try ets:lookup(Name, Key) of
+    [{_, Value, ExpireTime}] ->
+      if is_integer(ExpireTime) ->
+        case ExpireTime > current_time() of
+          true -> Value;
+          false ->
+            delete(Name, Key),
+            do_lookup(Name, Key, UpdateFun)
+        end;
+        true ->
+          Value
+      end;
+    [{_, _, _}] ->
+      delete(Name, Key),
+      do_lookup(Name, Key, UpdateFun);
+    [] ->
+      do_lookup(Name, Key, UpdateFun);
+    _ ->
+      Value
+  catch _:badarg ->
+    Value
+  end.
+
 
 -spec lookup(atom(), any()) -> {ok, any()} | error.
 lookup(Name, Key) ->
